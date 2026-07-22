494e21d2dbec4fa2595efb6a88a39848c726d271
knowledgebase21st <sghosh@AZAPXLPC1WN680.localdomain>
2026-07-22 15:59:10 -0700

    Added FirstNonRepeatedUsingHashMap

--- data-structure/big-o/FirstNonRepeatedUsingHashMap.java
+++ data-structure/big-o/FirstNonRepeatedUsingHashMap.java
@@ -30,3 +30,40 @@
  * ============================================================================
  */
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirstNonRepeatedUsingHashMap {
+
+    public static void main(String[] args) {
+
+        String text = "Programming";
+
+        Map<Character, Integer> frequencyMap = new HashMap<>();
+
+        // First Pass - Count frequencies
+        for (char ch : text.toCharArray()) {
+
+            frequencyMap.put(ch,
+                    frequencyMap.getOrDefault(ch, 0) + 1);
+        }
+
+        // Second Pass - Find first unique character
+        boolean found = false;
+
+        for (char ch : text.toCharArray()) {
+
+            if (frequencyMap.get(ch) == 1) {
+
+                System.out.println("First Non-Repeated Character : " + ch);
+
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("No Non-Repeated Character Found.");
+        }
+    }
+}
